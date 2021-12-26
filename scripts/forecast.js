90bfe94397a4389e66b3c53d1f4d36487d0c6f51
kstt27 <91312454+kstt27@users.noreply.github.com>
2021-12-27 01:25:12 +0800

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -3,7 +3,7 @@ const key = "Yo5CvJWAsJUdmCs2rhwvHyfy58oGNXjM";
 
 // Get weather info
 const getWeather = async (datakey) => {
-	const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+	const base = "https://dataservice.accuweather.com/currentconditions/v1/";
 	const query = `${datakey}?apikey=${key}`;
 	// await this promise until the result is out
 	const response = await fetch(base + query);
@@ -16,7 +16,7 @@ const getWeather = async (datakey) => {
 
 // Get city info
 const getCity = async (city) => {
-	const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+	const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
 	const query = `?apikey=${key}&q=${city}`;
 
 	const response = await fetch(base + query);
